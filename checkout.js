453b12d830562a2c5017828dd463b398451dbce7
Kay Krewson <kay.k@comcast.net>
2017-03-08 08:51:59 -0800

    filled out list of states, added .validate library

---
+++ checkout.js
@@ -0,0 +1,73 @@
+
+// SHIPPING
+// shipping-first
+
+
+// shipping-last
+
+
+// shipping-company
+
+
+// shipping-address-1
+
+
+// shipping-address-2
+
+
+// shipping-state
+
+
+// shipping-state
+
+
+// shipping-zip
+
+
+// shipping-copy-id
+
+
+// BILLING
+// billing-first
+
+
+// billing-last
+
+
+// billing-company
+
+
+// billing-address-1
+
+
+// billing-address-2
+
+
+// billing-state
+
+
+// billing-zip
+
+
+// billing-copy-id
+
+
+// CC INFORMATION
+// cc-number
+
+
+// cc-expiry
+
+
+// cc-cvc
+$('cc.cvc').validate ({
+  rules: {
+    field: {
+      required: true,
+      maxlength: 3
+    }
+  }
+}
+
+
+// SUBMIT
