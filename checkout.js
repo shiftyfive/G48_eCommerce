aadc2c36b8506e811626bbfeea1a2560db3f9fc8
Kay Krewson <kay.k@comcast.net>
2017-03-08 13:02:04 -0800

    adding bootstrap classes to the form inputs to clean up the layout

--- checkout.js
+++ checkout.js
@@ -1,64 +1,70 @@
 
-$().ready(function() {
-  $('#purchase').validate({
-    console.log('here now')
-    rules: {
-      shipping-first: {
-        required: true
-      },
-      shipping-last: {
-        required: true
-      },
-      shipping-address-1: {
-        required: true
-      },
-      shipping-address-2: {
-        required: true
-      },
-      shipping-zip: {
-        required: true,
-        minlength: 5
-      },
-      billing-first: {
-        required: true
-      },
-      billing-last: {
-        required: true
-      },
-      billing-address-1: {
-        required: true
-      },
-      billing-address-2: {
-        required: true
-      },
-      billing-zip: {
-        required: true,
-        minlength: 5
-      }
-    } // end rules
-  }) // $('#purchase').validate
-}) // end $().ready(function()
+// $().ready(function() {
+  console.log('here now')
 
 
-// billing-copy-id
-
-
-// CC INFORMATION
-// cc-number
-
-
-// cc-expiry
-
-
-// cc-cvc
-$('cc.cvc').validate ({
-  rules: {
-    field: {
-      required: true,
-      maxlength: 3
-    }
-  }
-}
+  // $('#purchase').validate({
+  //   rules: {
+  //     shipping-first: {
+  //       required: true
+  //     },
+  //     shipping-last: {
+  //       required: true
+  //     },
+  //     shipping-address-1: {
+  //       required: true
+  //     },
+  //     shipping-address-2: {
+  //       required: true
+  //     },
+  //     shipping-zip: {
+  //       required: true,
+  //       minlength: 5
+  //     },
+  //     billing-first: {
+  //       required: true
+  //     },
+  //     billing-last: {
+  //       required: true
+  //     },
+  //     billing-address-1: {
+  //       required: true
+  //     },
+  //     billing-address-2: {
+  //       required: true
+  //     },
+  //     billing-zip: {
+  //       required: true,
+  //       minlength: 5
+  //     },
+  //     cc-number: {
+  //       required: true
+  //     }
+  //     cc-expiry {
+  //       required: true
+  //     }
+  //     cc-cvc {
+  //       required: true,
+  //       minlength: 3,
+  //       maxlength: 3
+  //     }
+  //   }, // end rules
+  //
+  //   messages: {
+  //     shipping-first: "Enter first name"
+  //     shipping-last: "Enter last name"
+  //     shipping-address-1: "Enter address"
+  //     shipping-zip: "Enter zip code"
+  //     billing-first: "Enter first name"
+  //     billing-last: "Enter last name"
+  //     billing-address-1: "Enter address"
+  //     billing-zip: "Enter zip code"
+  //     cc-number: "Enter credit card number"
+  //     cc-expiry "Enter expiration date"
+  //     cc-cvc: "Enter 3-digit CVC"
+  //   } // end messages
+//   }) // end $('#purchase').validate
+// }) // end $().ready(function()
 
 
 // SUBMIT
